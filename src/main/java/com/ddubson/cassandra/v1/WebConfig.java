a1ac9fb51726f5024fb2676b57a71929f3fd85fc
Dmitriy Dubson <d.dubson@gmail.com>
2016-03-09 21:58:29 -0500

    Added basic index.html file as a placeholder

---
+++ src/main/java/com/ddubson/cassandra/v1/WebConfig.java
@@ -0,0 +1,15 @@
+package com.ddubson.cassandra.v1;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ddubson on 3/9/16.
+ */
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward root to webapp/index.html
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
