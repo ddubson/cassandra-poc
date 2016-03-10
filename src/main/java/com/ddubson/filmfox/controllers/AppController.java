8a05834fe8c0388235c2bf32f9ee21ffd5599cc9
Dmitriy Dubson <d.dubson@gmail.com>
2016-03-10 17:36:22 -0500

    Added simple endpoint to add movies to Cassandra key space

--- src/main/java/com/ddubson/filmfox/controllers/AppController.java
+++ src/main/java/com/ddubson/filmfox/controllers/AppController.java
@@ -1,9 +1,12 @@
 package com.ddubson.filmfox.controllers;
 
+import com.datastax.driver.core.utils.UUIDs;
 import com.ddubson.filmfox.models.Movie;
 import com.ddubson.filmfox.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,14 +17,25 @@ import java.util.List;
  */
 @RestController
 public class AppController {
-	@Autowired
-	MovieRepository movieRepository;
+    @Autowired
+    MovieRepository movieRepository;
 
-	@RequestMapping("/movies")
-	public List<Movie> index() {
-		Iterable<Movie> movies = movieRepository.findAll();
-		List<Movie> movieList = new ArrayList<>();
-		movies.forEach(movieList::add);
-		return movieList;
-	}
+    @RequestMapping(value = "/movies", method = RequestMethod.GET)
+    public List<Movie> listMovies() {
+        Iterable<Movie> movies = movieRepository.findAll();
+        List<Movie> movieList = new ArrayList<>();
+        movies.forEach(movieList::add);
+        return movieList;
+    }
+
+    @RequestMapping(value = "/movies", method = RequestMethod.POST)
+    public Movie addMovie(@RequestBody Movie movieJson) {
+        Movie movie = new Movie();
+        movie.setId(UUIDs.timeBased());
+        movie.setDirectedBy(movieJson.getDirectedBy());
+        movie.setName(movieJson.getName());
+        movie.setYearReleased(movieJson.getYearReleased());
+        movieRepository.save(movie);
+        return movie;
+    }
 }
