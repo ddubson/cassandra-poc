6ade9631e2a1206db523aaf093fb4a6951ce3a8d
Dmitriy Dubson <d.dubson@gmail.com>
2016-03-28 20:40:22 -0400

    Add 404 message for search result endpoint

--- src/main/webapp/js/MoviesModule/SearchMoviesController.js
+++ src/main/webapp/js/MoviesModule/SearchMoviesController.js
@@ -2,7 +2,8 @@ angular.module('MoviesModule').controller('SearchMoviesController',
 function($scope, $location, $http) {
     $scope.search = function() {
         if($scope.movieToFind) {
-            $http.post("/movies/search", $scope.movieToFind).success(function(response) {
+            $http.post("/movies/search", $scope.movieToFind)
+            .success(function(response) {
                 $scope.movieResults = response;
             });
         }
